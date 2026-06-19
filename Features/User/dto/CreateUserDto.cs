13fb1d9e0aa84b80acafca395fb3ffedec5230ac
Geovane Fiirst <geovanefiirst@hotmail.com>
2026-06-19 10:01:47 -0400

    feat: criando dto para criação de usuario

---
+++ Features/User/dto/CreateUserDto.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+public class CreateUserDto
+{
+    [Required(ErrorMessage = "O nome é obrigatorio")]
+    [MaxLength(
+        100,
+        ErrorMessage = "{0} deve conter no maximo {1} caracteres."
+    )]
+    public string Name {get; set;} = "";
+
+    [Required(ErrorMessage = "O email é obrigatorio")]
+    [MaxLength(
+        255,
+        ErrorMessage = "{0} deve conter no maximo {1} caracteres."
+    )]
+    public string Email {get; set;} = "";
+
+    [Required(ErrorMessage = "A senha é obrigatoria")]
+    [MaxLength(
+        255,
+        ErrorMessage = "{0} deve conter no maximo {1} caracteres."
+    )]
+    public string Password {get; set;} = "";
+}

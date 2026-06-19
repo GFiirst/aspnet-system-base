f7b15a55c6ebfb7c181b154d2171ee1695f7b240
Geovane Fiirst <geovanefiirst@hotmail.com>
2026-06-19 08:02:48 -0400

    feat: configurando campos do refreshtoken

--- Features/auth/entities/RefreshToken.cs
+++ Features/auth/entities/RefreshToken.cs
@@ -1,16 +1,16 @@
 public class RefreshToken : BaseEntity
 {
-    public string Token {get; set;} = "";
+    public string TokenHash {get; set;} = "";
 
     public DateTime ExpiredAt{get; set;}
 
     public TokenStatusEnum Status {get; set;}
 
-    public string UserAgent {get; set;} = "";
+    public string? UserAgent {get; set;}
 
-    public string Device {get; set;} = "";
+    public string? Device {get; set;}
 
-    public string Ip {get; set;} = "";
+    public string? Ip {get; set;}
 
     public Guid UserId { get; set; }
 
--- Features/auth/entities/RefreshTokenConfiguration.cs
+++ Features/auth/entities/RefreshTokenConfiguration.cs
@@ -9,8 +9,28 @@ public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
     {
         builder.ToTable("refresh_token");
 
+        builder.HasIndex(x => x.TokenHash)
+            .IsUnique();
+
+        builder.Property(x => x.TokenHash)
+            .HasMaxLength(255);
+        
         builder.Property(x => x.Status)
             .HasConversion<string>()
             .HasMaxLength(20);
+
+        builder.Property(x => x.Device)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.UserAgent)
+            .HasMaxLength(600);
+
+        builder.Property(x => x.Ip)
+            .HasMaxLength(45);
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.RefreshTokens)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }

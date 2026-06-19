b30b84663ae08903bec64337ae8fe74f1c34fdb0
Geovane Fiirst <geovanefiirst@hotmail.com>
2026-06-19 08:38:06 -0400

    feat: ajustando nome de colunas do refreshtoken

--- Features/auth/entities/RefreshTokenConfiguration.cs
+++ Features/auth/entities/RefreshTokenConfiguration.cs
@@ -13,24 +13,35 @@ public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
             .IsUnique();
 
         builder.Property(x => x.TokenHash)
+            .HasColumnName("token_hash")
             .HasMaxLength(255);
+
+        builder.Property(x => x.ExpiredAt)
+            .HasColumnName("expired_at");
         
         builder.Property(x => x.Status)
+            .HasColumnName("status")
             .HasConversion<string>()
             .HasMaxLength(20);
 
         builder.Property(x => x.Device)
+            .HasColumnName("device")
             .HasMaxLength(100);
 
         builder.Property(x => x.UserAgent)
+            .HasColumnName("user_agent")
             .HasMaxLength(600);
 
         builder.Property(x => x.Ip)
+            .HasColumnName("ip")
             .HasMaxLength(45);
 
         builder.HasOne(x => x.User)
             .WithMany(x => x.RefreshTokens)
             .HasForeignKey(x => x.UserId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(x => x.UserId)
+            .HasColumnName("user_id");
     }
 }

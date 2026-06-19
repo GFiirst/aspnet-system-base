772a0e0d1490cba5d6e743d892cd263139ad6ac0
Geovane Fiirst <geovanefiirst@hotmail.com>
2026-06-19 08:34:37 -0400

    feat: criando user configuration

---
+++ Features/User/UserConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(
+        EntityTypeBuilder<User> builder
+    ){
+        builder.ToTable("user");
+
+        builder.Property(x => x.Name)
+        .HasColumnName("name")
+        .HasMaxLength(100);
+
+        builder.HasIndex(x => x.Email)
+        .IsUnique()
+        .HasFilter("deleted_at IS NULL");
+
+        builder.Property(x => x.Email)
+        .HasColumnName("email")
+        .HasMaxLength(255);
+
+        builder.Property(x => x.Password)
+        .HasColumnName("password")
+        .HasMaxLength(255);
+    }
+}

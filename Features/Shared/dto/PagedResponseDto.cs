416fa3b4a3383443401021317872ca95e54eba97
Geovane Fiirst <geovanefiirst@hotmail.com>
2026-08-20 14:27:50 -0400

    feat: adicionando resposta genérica para paginação

---
+++ Features/Shared/dto/PagedResponseDto.cs
@@ -0,0 +1,10 @@
+public class PagedResponseDto<T>
+{
+    public List<T> Data { get; set; } = [];
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages => PageSize > 0
+        ? (int)Math.Ceiling((double)TotalCount / PageSize)
+        : 0;
+}
